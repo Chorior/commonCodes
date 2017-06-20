35e4e70e3d91bf5028c390c90dabc183bb059791
PengZhen <pengzhen.chorior@gmail.com>
2017-06-20 17:26:40 +0800

    types.h

---
+++ C++/socketTCP/windows/server/types.h
@@ -0,0 +1,14 @@
+#ifndef _TYPE_H_
+#define _TYPE_H_
+
+// some types
+typedef int I4;
+typedef short I2;
+typedef char I1;
+typedef unsigned int U4;
+typedef unsigned short U2;
+typedef unsigned char U1;
+typedef double F8;
+typedef float F4;
+
+#endif
